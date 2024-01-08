d41169426bd624302018fcf2dafe598475147859
Vinit Lathiwala <114902266+Vinitv38@users.noreply.github.com>
2024-01-08 17:21:59 +0530

    Add files via upload

---
+++ Portfolio webpage/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+// setTimeout(function() {
+//     $('.about-info-inner').typed({
+//       strings: [
+//         "<span>Hi, I am Vinit Lathiwala</span>  currently pursuing a Bachelor's in Computer Science specialized in Artificial Intelligence and Machine Learning at Vellore Institute of Technology, Bhopal. With a deep-rooted interest in mathematical logic and physics since my early years, I'm now passionate about applying these foundations to the field of machine learning.<br><br>I thrive on challenges and continuous learning, fostering a positive mindset to tackle problems while relishing collaborative teamwork for project development. While machine learning remains my primary focus, I'm equally eager to explore diverse domains and broaden my horizons across various fields.<br><br>Beyond technology, my curiosity extends to space science and astronomy—an area that constantly captivates my attention, compelling me to expand my knowledge in this fascinating realm. In my leisure, I find joy in sporting activities such as badminton, cricket, and football. Additionally, I indulge in pursuits like listening to music, sketching, driving, traveling, playing games, and immersing myself in space-time content through reading."
+//       ],
+//       typeSpeed: 7,
+//       contentType: 'html'
+//     });
+//   },100);
